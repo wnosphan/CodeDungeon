bdf9563a1337ef8e9c1236867f51a25d3106fab1
ya3k <truongdinhvan1233@gmail.com>
2024-07-07 14:21:19 +0700

    add cam

--- Assets/Scripts/RankingScript.cs
+++ Assets/Scripts/RankingScript.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RankingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
